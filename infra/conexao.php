9cc83125922354ccf9e8a63fc0d43ea003728bba
Nicolas Lopes <nicolas_s_lopes@estudante.sesisenai.org.br>
2026-08-20 11:24:13 -0300

    Update conexao.php

--- infra/conexao.php
+++ infra/conexao.php
@@ -6,10 +6,10 @@
     $database = "sistema_prato";
     $porta = "3306";
 
-    $conexao = mysqli_connect($host, $user, $password, $database, $porta);
+    $conexao = nem mysqli($host, $user, $password, $database, $porta);
 
-    if (!$conexao) {
-        die("Falha na conexão: " . mysqli_connect_error());
+    if ($conexao->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);
     }
 
     $conexao->set_charset("utf8");
